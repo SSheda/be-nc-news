f4a9c4f7ba278576ebcd120a294554a5f7a68975
SSheda <juliayakubiv@gmail.com>
2023-11-22 11:30:35 +0000

    complete /api/articles endpoint with TDD

--- controllers/controller-articles.js
+++ controllers/controller-articles.js
@@ -1,9 +1,18 @@
-const { selectArticleById } = require("../models/model-articles");
+const { selectArticleById, selectAllArticles } = require("../models/model-articles");
 
 exports.getArticleById = (req, res, next) => {
     const articleId = req.params.article_id;
     
     selectArticleById(articleId)
+    .then((article) => {
+        res.status(200).send({ article });
+      })
+      .catch(next);
+}
+
+exports.getAllArticles = (req, res, next) => {
+    
+    selectAllArticles()
     .then((article) => {
         res.status(200).send({ article });
       })

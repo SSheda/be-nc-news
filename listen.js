4158db7b88a1c1e921de4f7bfb68029eb2d78011
SSheda <juliayakubiv@gmail.com>
2023-11-24 10:22:58 +0000

    add link connection to DB and make changes to listen and json files

--- listen.js
+++ listen.js
@@ -1,5 +1,5 @@
 const app = require("./app");
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, () => {
-    console.log("listening port 9090");
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
